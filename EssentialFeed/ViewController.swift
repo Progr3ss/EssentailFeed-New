dc0dd84f98a5ff8c23f2e5217cf44774f0dafb6a
Martin Chibwe <mchibwe01@gmail.com>
2022-05-12 20:46:48 -0700

    Initial Commit

---
+++ EssentialFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialFeed
+//
+//  Created by Martin Chibwe on 5/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
