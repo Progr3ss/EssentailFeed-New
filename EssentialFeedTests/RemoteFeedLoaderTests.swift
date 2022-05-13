e393e98ad06b9e61fe3f7547721c2a9ba30cc84c
Martin Chibwe <mchibwe01@gmail.com>
2022-05-12 22:45:20 -0700

    moving code to production for download data

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -17,7 +17,7 @@ class RemoteFeedLoader {
     }
     
     func load() {
-        client.get(from: URL(string: "https://a-url.com")!)
+        client.get(from: url)
     }
 }
 protocol HTTPClient {
