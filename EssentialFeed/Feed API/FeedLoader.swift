5dfe394411bbfdf213f4ba38080d7d29af36ed74
Martin Chibwe <mchibwe01@gmail.com>
2023-04-27 22:34:11 -0700

    add comments

--- EssentialFeed/Feed API/FeedLoader.swift
+++ EssentialFeed/Feed API/FeedLoader.swift
@@ -7,30 +7,38 @@
 
 import Foundation
 
+// RemoteFeedLoader is responsible for loading feed from remote server
 public final class RemoteFeedLoader {
     private let client: HTTPClient
     private let url: URL
     
+    // Possible errors that can occur while loading the feed
     public enum Error: Swift.Error {
         case connectivity
         case invalidData
     }
     
+    // Possible results of loading the feed
     public enum Result: Equatable {
         case success([FeedItem])
         case failure(Error)
     }
     
+    // Initialize new instance of RemoteFeedLoader
     public init(url: URL, client: HTTPClient) {
         self.client = client
         self.url = url
     }
     
+    // Loads the feed from the remote server and calls the completion handler with the result.
     public func load(completion: @escaping (Result) -> Void) {
         client.get(from: url) { [weak self] result in
+            // Guard against the possibility that the object has been deallocated.
             guard self != nil else {return}
             
+            // Handle the result of the request
             switch result {
+                
             case let .success(data, response):
                     completion(FeedItemsMapper.map(data, from: response))
             case .failure:
